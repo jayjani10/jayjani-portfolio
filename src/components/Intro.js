cf76efca92a7acbd6e46baf9cf70eaf31b9053f2
Jay <jayjani008@gmail.com>
2021-11-15 23:40:17 +0530

    Added animation to intro component using framer motion.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import Me from '../assets/Images/profile-img.png';
+import { motion } from "framer-motion";
+import Me from "../assets/Images/profile-img.png";
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   position: absolute;
   left: 50%;
   top: 50%;
@@ -14,12 +15,12 @@ const Box = styled.div`
 
   background: linear-gradient(
     to right,
-    ${props => props.theme.body} 50%,
-    ${props => props.theme.text} 50% )bottom,
+    ${(props) => props.theme.body} 50%,
+    ${(props) => props.theme.text} 50% )bottom,
     linear-gradient(
         to right,
-        ${props => props.theme.body} 50%,
-        ${props => props.theme.text} 50% )top;
+        ${(props) => props.theme.body} 50%,
+        ${(props) => props.theme.text} 50% )top;
   background-repeat: no-repeat;
   background-size: 100% 2px;
   border-left: 2px solid ${(props) => props.theme.body};
@@ -29,51 +30,60 @@ const Box = styled.div`
 `;
 
 const SubBox = styled.div`
-width: 50%;
-position: relative;
-display: flex;
-.pic{
+  width: 50%;
+  position: relative;
+  display: flex;
+  .pic {
     position: absolute;
     bottom: 0;
     left: 50%;
     transform: translate(-50%, 0%);
     width: 100%;
     height: auto;
-}
-`
+  }
+`;
 
 const Text = styled.div`
-font-size: calc(1em + 1.5vw);
-color: ${props => props.theme.body};
-padding: 2rem;
-cursor: pointer;
-display: flex;
-flex-direction: column;
-justify-content: space-evenly;
+  font-size: calc(1em + 1.5vw);
+  color: ${(props) => props.theme.body};
+  padding: 2rem;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 
-&>*:last-child{
-    color: ${props => `rgba(${props.theme.bodyRgba}, 0.6)`};
+  & > *:last-child {
+    color: ${(props) => `rgba(${props.theme.bodyRgba}, 0.6)`};
     font-size: calc(0.5rem + 1.5vw);
     font-weight: 300;
-}
-
+  }
 `;
 
 const Intro = () => {
-  return (<Box>
+  return (
+    <Box
+      initial={{ height: 0 }}
+      animate={{ height: "55vh" }}
+      transition={{ type: "easein", duration: 0.5, delay: 1 }}
+    >
       <SubBox>
-          <Text>
-              <h1>Hi,</h1>
-              <h3>I'm Jay.</h3>
-              <h6>I design simple yet beautiful websites.</h6>
-          </Text>
+        <Text>
+          <h1>Hi,</h1>
+          <h3>I'm Jay.</h3>
+          <h6>I design simple yet beautiful websites.</h6>
+        </Text>
       </SubBox>
       <SubBox>
-        <div>
-            <img className="pic" src={Me} alt="Profile Pic"/>
-        </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5  }}
+        >
+          <img className="pic" src={Me} alt="Profile Pic" />
+        </motion.div>
       </SubBox>
-  </Box>);
+    </Box>
+  );
 };
 
 export default Intro;
--- src/components/Main.js
+++ src/components/Main.js
@@ -15,8 +15,9 @@ const MainContainer = styled.div`
   overflow: hidden;
   position: relative;
 
+  h1,
   h2,
-  h2,
+  h3,
   h4,
   h5,
   h6 {
