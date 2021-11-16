e1598ac5b8225aee8a42aca040f33d6cf86f050f
Jay <jayjani008@gmail.com>
2021-11-16 23:49:31 +0530

    Added motion effects on menu items.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import styled, { keyframes } from "styled-components";
 import { NavLink } from "react-router-dom";
 import LogoComponent from "../subComponents/LogoComponent";
@@ -149,24 +150,26 @@ const Main = () => {
         </Center>
       </Container>
 
-      {click ? <Intro click={click}/> : null}
+      {click ? <Intro click={click} /> : null}
 
       <Contact target="_blank" to={{ pathname: "mailto:jayjani008@gmail.com" }}>
-        <h2>Say hi...</h2>
+        <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          Say hi...
+        </motion.h2>
       </Contact>
       <Blog to="/blog">
-        <h2>Blog</h2>
+        <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>Blog</motion.h2>
       </Blog>
       <Work to="/work" click={click}>
-        <h2>Work</h2>
+        <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>Work</motion.h2>
       </Work>
 
       <BottomBar>
         <About to="/about" click={click}>
-          <h2>About.</h2>
+          <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>About.</motion.h2>
         </About>
         <Skills to="/skills">
-          <h2>My Skills.</h2>
+          <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>My Skills.</motion.h2>
         </Skills>
       </BottomBar>
     </MainContainer>
