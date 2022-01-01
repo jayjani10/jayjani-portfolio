3978a96d0ca349e31cf73f71237eca90bdf98e74
Jay <jayjani008@gmail.com>
2022-01-01 23:50:16 +0530

    Started working on Skills Page. Created Layout.

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -6,7 +6,6 @@ import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
 import { Blogs } from "../data/BlogData";
 import BlogComponent from "./BlogComponent";
-import AnchorComponent from "../subComponents/Anchor";
 
 const MainContainer = styled.div`
   background-image: url(${img});
@@ -44,7 +43,6 @@ const BlogPage = () => {
         <LogoComponent />
         <PowerButton />
         <SocialIcons />
-        <AnchorComponent />
         <Center>
           <Grid>
             {Blogs.map((blog) => {
